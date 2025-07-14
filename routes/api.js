f8d059ce22f8a4433806ef12ad3e0283733e9bf2
anunyaOrange <anunyaOrange@proton.me>
2025-07-14 14:38:13 +0700

    ok

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,19 @@
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+  .get((req, res) => {
+    res.json({ message: 'GET threads for board: ' + req.params.board });
+  })
+  .post((req, res) => {
+    res.json({ message: 'POST thread to board: ' + req.params.board });
+  })
+  .put((req, res) => {
+    res.json({ message: 'PUT thread on board: ' + req.params.board });
+  })
+  .delete((req, res) => {
+    res.json({ message: 'DELETE thread from board: ' + req.params.board });
+  });
     
   app.route('/api/replies/:board');
 
