13c635d4b1ef33d9bc3f4dd2b7366dfeb7de6cca
anunyaOrange <anunyaOrange@proton.me>
2025-07-22 12:37:47 +0700

    add uuid and nanoid

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,18 @@
 'use strict';
 
+const { nanoid } = require("nanoid");
+const { v4: uuidv4 } = require('uuid');
+
 const db = [{
-  id: 1,
+  uuidid: uuidv4(),
+  nanoid: nanoid(),
   board: 'test-board',
   delete_password: '123456',
   text: 'This is a sample thread text',
   created_on: new Date(),
   replies: [{
-    id: 1,
+    uuidid: uuidv4(),
+    nanoid: nanoid(),
     text: 'This is a sample reply',
     delete_password: '123456',
     created_on: new Date(),
@@ -21,6 +26,7 @@ module.exports = function (app) {
     res.json({ message: 'GET threads for board: ' + req.params.board });
   })
   .post((req, res) => {
+    console.log("pre db: ", db);
     db.push(`${db.length + 1} - ${req.body.text} on board ${req.params.board}`);
     res.json({ message: 'POST thread to board: ' + req.params.board + ' => ' + JSON.stringify(req.body) + ' => ' + db });
   })
