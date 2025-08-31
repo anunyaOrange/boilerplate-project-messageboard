e0e4f6c14920fee2a4582dca4beaf51bf1ba03c2
anunyaOrange <anunyaOrange@proton.me>
2025-08-31 10:54:00 +0700

    add bummped on

--- routes/api.js
+++ routes/api.js
@@ -19,6 +19,7 @@ const { v4: uuidv4 } = require('uuid');
 //   }],
 // }];
 
+const newDate = new Date();
 const db = [
   {
     board: 'example',
@@ -27,42 +28,56 @@ const db = [
         _id: uuidv4(),
         delete_password: 'xxxxx',
         text: 'This is example text',
-        created_on: new Date(),
+        created_on: newDate,
+        bumped_on: newDate,
+        reported: false,
         replies: [{
           _id: uuidv4(),
           text: 'This is example reply',
           delete_password: 'xxxxx',
-          created_on: new Date(),
+          created_on: newDate,
+          bumped_on: newDate,
+          reported: false,
         }],
       },
       {
         _id: '3650e13f-820c-4365-b127-29497b4fa93f',
         delete_password: '123456',
         text: 'This thread for delete',
-        created_on: new Date(),
+        created_on: newDate,
+        bumped_on: newDate,
+        reported: false,
         replies: [{
           _id: uuidv4(),
           text: 'This is example reply',
           delete_password: 'xxxxx',
-          created_on: new Date(),
+          created_on: newDate,
+          bumped_on: newDate,
+          reported: false,
         }],
       },
       {
         _id: 'f250e13f-820c-4365-b127-29497b4fa9f2',
         delete_password: '123456',
         text: 'This thread for replies',
-        created_on: new Date(),
+        created_on: newDate,
+        bumped_on: newDate,
+        reported: false,
         replies: [{
           _id: 'a250e13f-820c-4365-b127-29497b4fa9fa',
           text: 'This reply for delete',
           delete_password: '1234567',
-          created_on: new Date(),
+          created_on: newDate,
+          bumped_on: newDate,
+          reported: false,
         },
         {
           _id: 'b250e13f-820c-4365-b127-29497b4fa9fb',
           text: 'This reply for reporting',
           delete_password: '1234567890',
-          created_on: new Date(),
+          created_on: newDate,
+          bumped_on: newDate,
+          reported: false,
         }],
       }
     ],
@@ -98,11 +113,15 @@ module.exports = function (app) {
       const board = req.params.board;
       const boardDB = db.filter(b => b.board === board);
 
+      const curDate = new Date();
       const data = {
         tid: uuidv4(),
         delete_password: req.body.delete_password,
         text: req.body.text,
         created_on: new Date(),
+        created_on: curDate,
+        bumped_on: curDate,
+        reported: false,
         replies: [],
       };
 
@@ -162,11 +181,14 @@ module.exports = function (app) {
       const boardDB = db.filter(b => b.board === board);
       const { thread_id, text, delete_password } = req.body;
 
+      const curDate = new Date();
       const reply = {
         rid: uuidv4(),
         delete_password: delete_password,
         text: text,
-        created_on: new Date()
+        created_on: curDate,
+        bumped_on: curDate,
+        reported: false,
       };
 
       res.set('Content-Type', 'text/plain');
